1be01cff54564031dc5c4c5a579944ef4274738d
nob-aoki <nob-aoki@tomey.co.jp>
2026-02-13 12:33:19 +0900

    added commands.

--- src/argument.rs
+++ src/argument.rs
@@ -1,21 +1,33 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[command(version, author, about)]
 pub struct Arg {
+    /// mode
+    #[command(subcommand)]
+    pub md : Mode,
     /// kifu directory
-    #[arg(long)]
-    pub kifudir : Option<String>,
+    #[arg(long, global = true, value_delimiter=',')]
+    pub kifudir : Vec<String>,
     /// show progressbar
-    #[arg(long, default_value_t = false)]
+    #[arg(long, global = true, default_value_t = false)]
     pub progressbar : bool,
     /// log file path.
-    #[arg(long)]
+    #[arg(long, global = true)]
     pub log : Option<String>,
     /// get mate(N-1) positions by extracting mateN.
-    #[arg(long, short, default_value_t = 3)]
+    #[arg(long, short, global = true, default_value_t = 3)]
     pub  mate : u32,
     /// ruversi config file
-    #[arg(long)]
+    #[arg(long, global = true)]
     pub ru_config : Option<String>,
+    /// show details
+    #[arg(long, global = true, default_value_t=false)]
+    pub verbose : bool,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Mode {
+    Kifu,  // Extract from kifu files
+    Mate,  // Extract from mate files
 }
