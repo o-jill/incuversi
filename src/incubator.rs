4f609ef769551a64eee1283628effbdbd24dbe05
o-jill <nobaokix@gmail.com>
2026-06-04 09:23:09 +0900

    fixed regexp pattern.

--- src/incubator.rs
+++ src/incubator.rs
@@ -1202,7 +1202,7 @@ impl Incubator {
         if let Some(pb) = &pbtop {pb.inc(1);}  // 1
 
         // let valptn = Regex::new("[BW](-?[0-9.]+)").unwrap();
-        let valptn = Regex::new("[BW]([-+]?[0-9.]+)").unwrap();
+        let valptn = Regex::new(", [BW]([-+]?[0-9.]+)").unwrap();
         for d in self.kifudir.iter() {
             let files = data_loader::findfiles(&format!("./{d}"));
             if let Some(pb) = &pbtop {pb.inc(1);}  // 2n
